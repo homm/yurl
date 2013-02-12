f35b184b0deb32fe896757fb15bfc2e19406be1a
homm <homm86@gmail.com>
2013-02-12 12:59:05 +0400

    strip @ in userinfo introduce URL namedtuple with universal constructor port splitting in python

--- betterparse.py
+++ betterparse.py
@@ -1,4 +1,5 @@
 import re
+from collections import namedtuple
 
 # This module based on rfc3986.
 
@@ -22,11 +23,44 @@ import re
 _split_re = re.compile(r'''
     (?:([a-z][a-z0-9+\-.]*):)?  # scheme
     (?://                       # authority
-        ([^/?\#@\[\]]*@)?       # userinfo
-        ([^/?\#]*)              # host
-        (?::(\d*))?             # port
+        (?:([^/?\#@\[\]]*)@)?   # userinfo
+        ([^/?\#]*)              # host:port
     )?
     ([^?\#]*)                   # path
     \??([^\#]*)                 # query
     \#?(.*)                     # fragment
     ''', re.VERBOSE | re.IGNORECASE | re.DOTALL).match
+
+URLBase = namedtuple('URLBase', 'scheme host path query fragment userinfo port')
+
+class URL(URLBase):
+    __slots__ = ()
+
+    def __new__(cls, url=None, scheme='', host='', path='', query='',
+                fragment='', userinfo='', port=''):
+        if url is not None:
+            (scheme, userinfo, host,
+             path, query, fragment) = _split_re(url).groups('')
+
+            # We can not match port number in regexp. Host itself can contain
+            # digits and ":"
+            _port_idx = host.rfind(':')
+            if _port_idx >= 0:
+                _port = host[_port_idx + 1:]
+                if not _port or _port.isdigit():
+                    host, port = host[:_port_idx], _port
+
+        # | Although schemes are case-insensitive, the canonical form
+        # | is lowercase. An implementation should only produce lowercase
+        # | scheme names for consistency.
+        scheme = scheme.lower()
+
+        # | Although host is case-insensitive, producers and normalizers
+        # | should use lowercase for registered names and hexadecimal
+        # |addresses for the sake of uniformity.
+        # TODO: | while only using uppercase letters for percent-encodings
+        host = host.lower()
+
+        return tuple.__new__(cls, (scheme, host, path, query, fragment,
+                                   userinfo, port))
+
--- tests.py
+++ tests.py
@@ -1,20 +1,42 @@
 import unittest
 
-from betterparse import _split_re
+from betterparse import URL
 
 
 class SplitTests(unittest.TestCase):
     def one_try(self, url, scheme='', host='', path='', query='', fragment='',
-            userinfo='', port=''):
-        splitted = (scheme, userinfo, host, port, path, query, fragment)
-        self.assertEqual(_split_re(url).groups(''), splitted)
+                userinfo='', port=''):
+        splitted = (scheme, host, path, query, fragment, userinfo, port)
+        self.assertEqual(URL(url), splitted)
 
     def test_scheme(self):
         self.one_try('scheme:path', 'scheme', '', 'path')
         self.one_try('scheme+with-allow.chars-33:path',
                      'scheme+with-allow.chars-33', '', 'path')
+
         self.one_try('not_a_cheme:path', '', '', 'not_a_cheme:path')
         self.one_try('37signals:books', '', '', '37signals:books')
+        self.one_try('lucku-number:33', 'lucku-number', '', '33')
+
+        self.one_try('HTTP:PATH', 'http', '', 'PATH')
+
+    def test_authority(self):
+        # Host starts with //
+        self.one_try('scheme://host/path', 'scheme', 'host', '/path')
+        self.one_try('scheme:/host/path', 'scheme', '', '/host/path')
+        self.one_try('scheme:///host/path', 'scheme', '', '/host/path')
+
+        # Host with empty parts
+        self.one_try('//host/path', '', 'host', '/path')
+        self.one_try('//@host:/path', '', 'host', '/path')
+
+        # port
+        self.one_try('//host:80/path', '', 'host', '/path', port='80')
+        self.one_try('//host:22:80/path', '', 'host:22', '/path', port='80')
+        self.one_try('//host:not/path', '', 'host:not', '/path')
+        self.one_try('//host:not:/path', '', 'host:not', '/path')
+        self.one_try('//host:22:not/path', '', 'host:22:not', '/path')
+        self.one_try('//host:22:not:/path', '', 'host:22:not', '/path')
 
 
 if __name__ == '__main__':
