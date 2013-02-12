7270bfe30bb6a0c265deaafa3e9b86dd5fb4b67a
homm <homm86@gmail.com>
2013-02-13 01:52:22 +0400

    ++SplitTests

--- tests.py
+++ tests.py
@@ -8,36 +8,85 @@ class SplitTests(unittest.TestCase):
                 userinfo='', port=''):
         splitted = (scheme, host, path, query, fragment, userinfo, port)
         self.assertEqual(URL(url), splitted)
+        self.assertEqual(URL(None, *splitted), splitted)
+        self.assertEqual(URL(None, *URL(url)), splitted)
 
-    def test_scheme(self):
+    def test_scheme_valid(self):
         self.one_try('scheme:path', 'scheme', '', 'path')
-        self.one_try('scheme+with-allow.chars-33:path',
-                     'scheme+with-allow.chars-33', '', 'path')
+        self.one_try('allow+chars-33.:path', 'allow+chars-33.', '', 'path')
+        self.one_try('simple:', 'simple', '', '')
+        self.one_try('google.com:80', 'google.com', '', '80')
+        self.one_try('google.com:80/root', 'google.com', '', '80/root')
 
+    def test_scheme_invalid(self):
         self.one_try('not_a_cheme:path', '', '', 'not_a_cheme:path')
         self.one_try('37signals:books', '', '', '37signals:books')
-        self.one_try('lucku-number:33', 'lucku-number', '', '33')
+        self.one_try(':realy-path', '', '', ':realy-path')
+        self.one_try('://even-this', '', '', '://even-this')
 
-        self.one_try('HTTP:PATH', 'http', '', 'PATH')
-
-    def test_authority(self):
-        # Host starts with //
+    def test_host_valid(self):
         self.one_try('scheme://host/path', 'scheme', 'host', '/path')
+        self.one_try('//host/path', '', 'host', '/path')
+        self.one_try('//host+path', '', 'host+path', '')
+        self.one_try('//host', '', 'host', '')
+        self.one_try('//HOST', '', 'host', '')
+        self.one_try('//this+is$also&host!', '', 'this+is$also&host!', '')
+
+    def test_host_invalid(self):
         self.one_try('scheme:/host/path', 'scheme', '', '/host/path')
         self.one_try('scheme:///host/path', 'scheme', '', '/host/path')
 
-        # Host with empty parts
-        self.one_try('//host/path', '', 'host', '/path')
-        self.one_try('//@host:/path', '', 'host', '/path')
-
-        # port
+    def test_port_valid(self):
         self.one_try('//host:80/path', '', 'host', '/path', port='80')
         self.one_try('//host:22:80/path', '', 'host:22', '/path', port='80')
-        self.one_try('//host:not/path', '', 'host:not', '/path')
-        self.one_try('//host:not:/path', '', 'host:not', '/path')
-        self.one_try('//host:22:not/path', '', 'host:22:not', '/path')
-        self.one_try('//host:22:not:/path', '', 'host:22:not', '/path')
+        self.one_try('//host:80', '', 'host', port='80')
+        self.one_try('//:80', port='80')
 
+    def test_port_invalid(self):
+        self.one_try('//host:no/path', '', 'host:no', '/path')
+        self.one_try('//host:22:no/path', '', 'host:22:no', '/path')
+        self.one_try('//host:-80/path', '', 'host:-80', '/path')
+
+    def test_userinfo_valid(self):
+        pass
+
+    def test_userinfo_invalid(self):
+        pass
+
+    def test_path_valid(self):
+        pass
+
+    def test_path_invalid(self):
+        pass
+
+    def test_query_valid(self):
+        pass
+
+    def test_query_invalid(self):
+        pass
+
+    def test_fragment_valid(self):
+        pass
+
+    def test_fragment_invalid(self):
+        pass
+
+    def test_case_sensitivity(self):
+        self.one_try('A://B:C@D.E/F?G#H', 'a', 'd.e', '/F', 'G', 'H', 'B:C')
+
+    def test_strip_empty_parts(self):
+        self.one_try('//@:?#')
+        self.one_try('///path', '', '', '/path')
+        self.one_try('//@host', '', 'host')
+        self.one_try('//host:', '', 'host')
+        self.one_try('//host:/', '', 'host', '/')
+        self.one_try('/', '', '', '/')
+        self.one_try('path', '', '', 'path')
+        self.one_try('/path', '', '', '/path')
+        self.one_try('/path?', '', '', '/path')
+        self.one_try('?')
+        self.one_try('/path#', '', '', '/path')
+        self.one_try('#')
 
 if __name__ == '__main__':
     unittest.main()
