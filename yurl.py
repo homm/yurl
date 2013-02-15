4692ebf4b8c12af581486e0fc4575947f624cfea
homm <homm86@gmail.com>
2013-02-15 11:01:12 +0400

    move re to class

--- yurl.py
+++ yurl.py
@@ -3,45 +3,46 @@ from collections import namedtuple
 
 # This module based on rfc3986.
 
-# Section 3.1. Scheme
-# | URI scheme specifications must define their own syntax so that
-# | all strings matching their scheme-specific syntax will also match
-# | the <absolute-URI> grammar:
-#
-# | absolute-URI  = scheme ":" hier-part [ "?" query ]
-#
-# As we can see, <absolute-URI> not require all scheme-specific
-# syntaxis to have fragment part. But as defined in section 2.2.,
-# "#" one of the generic delimiters. It also defined as delimeter
-# in many parts of rfc. As a result url can not contain char "#"
-# without quoting even if it scheme-specific syntax not use "#":
-#
-# | If data for a URI component would conflict with a reserved
-# | character's purpose as a delimiter, then the conflicting data
-# | must be percent-encoded before the URI is formed.
-#
-_split_re = re.compile(r'''
-    (?:([a-z][a-z0-9+\-.]*):)?  # scheme
-    (?://                       # authority
-        (?:([^/?\#@\[\]]*)@)?   # userinfo
-        ([^/?\#]*)()            # host:port
-    )?
-    ([^?\#]*)                   # path
-    \??([^\#]*)                 # query
-    \#?(.*)                     # fragment
-    ''', re.VERBOSE | re.IGNORECASE | re.DOTALL).match
 
 URLTuple = namedtuple('URLBase', 'scheme host path query fragment userinfo port')
 
 class URL(URLTuple):
     __slots__ = ()
 
+    # Section 3.1. Scheme
+    # | URI scheme specifications must define their own syntax so that
+    # | all strings matching their scheme-specific syntax will also match
+    # | the <absolute-URI> grammar:
+    #
+    # | absolute-URI  = scheme ":" hier-part [ "?" query ]
+    #
+    # As we can see, <absolute-URI> not require all scheme-specific
+    # syntaxis to have fragment part. But as defined in section 2.2.,
+    # "#" one of the generic delimiters. It also defined as delimeter
+    # in many parts of rfc. As a result url can not contain char "#"
+    # without quoting even if it scheme-specific syntax not use "#":
+    #
+    # | If data for a URI component would conflict with a reserved
+    # | character's purpose as a delimiter, then the conflicting data
+    # | must be percent-encoded before the URI is formed.
+    #
+    _split_re = re.compile(r'''
+        (?:([a-z][a-z0-9+\-.]*):)?  # scheme
+        (?://                       # authority
+            (?:([^/?\#@\[\]]*)@)?   # userinfo
+            ([^/?\#]*)()            # host:port
+        )?
+        ([^?\#]*)                   # path
+        \??([^\#]*)                 # query
+        \#?(.*)                     # fragment
+        ''', re.VERBOSE | re.IGNORECASE | re.DOTALL).match
+
     def __new__(cls, url=None, scheme='', host='', path='', query='',
                 fragment='', userinfo='', port=''):
         if url is not None:
             # All other arguments are ignored.
             (scheme, userinfo, host, port,
-             path, query, fragment) = _split_re(url).groups('')
+             path, query, fragment) = cls._split_re(url).groups('')
 
             # We can not match port number in regexp. Host itself can contain
             # digits and ":"
