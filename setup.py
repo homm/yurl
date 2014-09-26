74127c7366e3bfced6cbdff3df79f4a05632785f
homm <homm86@gmail.com>
2014-09-26 19:34:53 +0400

    read file as unicode should fix install on not utf-8 systems

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+import codecs
 from setuptools import setup
+
 
 tests_require = []
 if sys.version_info < (2, 7):
     tests_require.append('unittest2')
 
 setup(name='YURL',
-      version='0.12',
+      version='0.13',
       description='Yurl is alternative url manipulation library',
-      long_description=open('README.rst', 'r').read(),
+      long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
       author='Aleksadr Karpinsky',
       author_email='homm86@gmail.com',
       url='http://github.com/homm/yurl/',
